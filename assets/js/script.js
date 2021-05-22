18ad698e355e2704890f1e561035a627b76e6e61
LChinQuee <81491306+lchinquee@users.noreply.github.com>
2021-05-21 22:25:49 -0400

    initial styling & content

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner THEN the current day is displayed at the top of the calendar
+    // id = currentDay
+//Display current date
+var day = moment().format('dddd, MMMM Do');
+$('#currentDay').append(day);
+
+// WHEN I scroll down THEN I am presented with time blocks for standard business hours
+
+
+// WHEN I view the time blocks for that day THEN each time block is color-coded to indicate whether it is in the past, present, or future
+var currentTime = moment().format('LTS');
+console.log(currentTime);
+
+
+// WHEN I click into a time block THEN I can enter an event
+// WHEN I click the save button for that time block THEN the text for that event is saved in local storage
+// WHEN I refresh the page THEN the saved events persist
+// Can edit saved events
