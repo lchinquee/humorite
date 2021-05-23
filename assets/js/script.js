ac5db622e97efc5851c0a85386850c559840de42
LChinQuee <81491306+lchinquee@users.noreply.github.com>
2021-05-23 14:47:46 -0400

    add task edit and save functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,31 +2,84 @@
 var day = moment().format('dddd, MMMM Do');
 $('#currentDay').append(day);
 
-
-// WHEN I view the time blocks for that day THEN each time block is color-coded to indicate whether it is in the past, present, or future
 //Get current time in 24 hour clock
-var currentTime = moment().format('HH:mm');
-console.log(currentTime);
+var moment = moment().startOf('hour');
+var currentTime = moment.format('HH:mm');
 
 //Determine if each time block is in the past, present or future
-var startTime = [9, 10, 11 ,12, 13, 14, 15, 16, 17];
+var startTime = ["09:00", "10:00", "11:00" ,"12:00", "13:00", "14:00", "15:00", "16:00", "17:00"];
+var idTime = ["#nine", "#ten", "#eleven", "#twelve", "#thirteen", "#fourteen", "#fifteen", "#sixteen", "#seventeen"];
+//Associate the current time with a time string in the start time array
+var currentTimeIndex = startTime.indexOf(currentTime);
 
-$.each(startTime, function(i, value) {
-    timeInitial = startTime[i]++;
-    timeFinal = startTime[i]--;
 
-    console.log(i, value);
-    console.log(timeInitial);
-    console.log(timeFinal);
+$.each(startTime, function(i) {
+    //Present
+    if (i === currentTimeIndex) {
+        $(idTime[i]).removeClass('past future').addClass('present');
+    }
+    //Future
+    else if (i > currentTimeIndex) {
+        $(idTime[i]).removeClass('past present').addClass('future');
+    }
+    //Past
+    else if (i < currentTimeIndex) {
+        $(idTime[i]).removeClass('present future').addClass('past');
+    }
 });
 
+//Refresh page every 5 minutes
+$(document).ready(function() {
+    setInterval(function() {
+        window.location.reload();
+    }, 5 * 60 * 1000);
+});
 
-//Check current time every minute
+//Save buttons to save textarea content to local storage
+$("#btnNine").click(function() {
+    var task = $("#nine").val();
+    localStorage.setItem("#nine", JSON.stringify(task));
+});
+$("#btnTen").click(function() {
+    var task = $("#ten").val();
+    localStorage.setItem("#ten", JSON.stringify(task));
+});
+$("#btnEleven").click(function() {
+    var task = $("#eleven").val();
+    localStorage.setItem("#eleven", JSON.stringify(task));
+});
+$("#btnTwelve").click(function() {
+    var task = $("#twelve").val();
+    localStorage.setItem("#twelve", JSON.stringify(task));
+});
+$("#btnThirteen").click(function() {
+    var task = $("#thirteen").val();
+    localStorage.setItem("#thirteen", JSON.stringify(task));
+});
+$("#btnFourteen").click(function() {
+    var task = $("#fourteen").val();
+    localStorage.setItem("#fourteen", JSON.stringify(task));
+});
+$("#btnFifteen").click(function() {
+    var task = $("#fifteen").val();
+    localStorage.setItem("#fifteen", JSON.stringify(task));
+});
+$("#btnSixteen").click(function() {
+    var task = $("#sixteen").val();
+    localStorage.setItem("#sixteen", JSON.stringify(task));
+});
+$("#btnSeventeen").click(function() {
+    var task = $("#seventeen").val();
+    localStorage.setItem("#seventeen", JSON.stringify(task));
+});
 
-
-
-
-// WHEN I click into a time block THEN I can enter an event
-// WHEN I click the save button for that time block THEN the text for that event is saved in local storage
-// WHEN I refresh the page THEN the saved events persist
-// Can edit saved events
+//Content entered into textareas remain
+$("#nine").text(JSON.parse(localStorage.getItem("#nine")));
+$("#ten").text(JSON.parse(localStorage.getItem("#ten")));
+$("#eleven").text(JSON.parse(localStorage.getItem("#eleven")));
+$("#twelve").text(JSON.parse(localStorage.getItem("#twelve")));
+$("#thirteen").text(JSON.parse(localStorage.getItem("#thirteen")));
+$("#fourteen").text(JSON.parse(localStorage.getItem("#fourteen")));
+$("#fifteen").text(JSON.parse(localStorage.getItem("#fifteen")));
+$("#sixteen").text(JSON.parse(localStorage.getItem("#sixteen")));
+$("#seventeen").text(JSON.parse(localStorage.getItem("#seventeen")));
